d3e605607588f2ef37b88039fc2a95082eb0266f
utkarshraj0001 <utkarshrajpandey0001@gmail.com>
2025-09-28 02:19:16 +0530

    updated contract with independent registration mock verification

--- Contract/script/DeployRootstock.s.sol
+++ Contract/script/DeployRootstock.s.sol
@@ -1,32 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-
 import "forge-std/Script.sol";
-import {IdentityAttestations} from "../src/rootstock/IdentityAttestations.sol";
 import {PrizeEscrow} from "../src/rootstock/PrizeEscrow.sol";
 
-
 contract DeployRootstock is Script {
-function run() external {
-// pick up PRIVATE_KEY_RSK from .env automatically
-uint256 pk = vm.envUint("PRIVATE_KEY_RSK");
-vm.startBroadcast(pk);
+    function run() external {
+        // Option A: use env var
+        uint256 pk = vm.envUint("PRIVATE_KEY_RSK");
+        vm.startBroadcast(pk);
 
+        // If you prefer CLI key, comment the two lines above and use:
+        // vm.startBroadcast();
 
-// deploy IdentityAttestations (constructor may take a trusted sender; use address(0) for now)
-IdentityAttestations attest = new IdentityAttestations(address(0));
-
-
-// deploy PrizeEscrow with the attestations contract address
-PrizeEscrow escrow = new PrizeEscrow(address(attest));
-
-
-vm.stopBroadcast();
-
-
-console2.log("Rootstock testnet deploys:");
-console2.log("- IdentityAttestations:", address(attest));
-console2.log("- PrizeEscrow:", address(escrow));
-}
+        PrizeEscrow escrow = new PrizeEscrow();
+        vm.stopBroadcast();
+        console2.log("PrizeEscrow:", address(escrow));
+    }
 }
